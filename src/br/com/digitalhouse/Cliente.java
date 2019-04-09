78987b21eb33ce066613c249db323a9a9e5078a3
unknown <Digital@DH-A8-10.dhbrasil.digitalhouse.com>
2019-04-08 22:34:32 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Cliente.java
@@ -0,0 +1,50 @@
+package br.com.digitalhouse;
+
+public class Cliente {
+
+    private String nome;
+    private String sobrenome;
+    private String rg;
+    private String cpf;
+
+
+    public void setNome(String novoNome){
+        nome = novoNome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setSobrenome(String novoSobrenome){
+        sobrenome = novoSobrenome;
+    }
+
+    public String getSobrenome(){
+        return sobrenome;
+    }
+
+    public void setRg(String novoRg){
+        rg = novoRg;
+    }
+
+    public String getRg(){
+        return rg;
+    }
+
+    public void setCpf(String novoCpf){
+        cpf = novoCpf;
+
+    }
+
+    public String getCpf(){
+        return cpf:
+
+}
+
+
+
+
+
+
+
---
+++ src/br/com/digitalhouse/Conta.java
@@ -0,0 +1,11 @@
+package br.com.digitalhouse;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class Conta extends Cliente{
+
+    public boolean fazerDeposito(){
+        return true 
+    }
+
+}
